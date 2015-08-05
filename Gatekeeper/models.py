1ab0daabb5f69dc01218950919e07cc5a8202195
Chris Phillips <crphillip@hotmail.com>
2015-08-04 21:38:47 -0700

    Changing variable names to match Python conventions; adding new attributes to Dog()

--- Gatekeeper/models.py
+++ Gatekeeper/models.py
@@ -4,16 +4,16 @@
     import itertools
 
     _counter = itertools.count().__next__
-    _sizeRange = ['Small','Medium','Large']
+    _size_range = ['Small','Medium','Large']
     _sex = ['Male','Female']
-    _ageRange = ['Baby','Young','Adult','Senior']
+    _age_range = ['Baby','Young','Adult','Senior']
 
     def __init__(self):
         self.id = str(Animal._counter()).rjust(6, '0')
         self.name = ''
         self.age = 0
-        self.ageRange = '' #_ageRange[0]
-        self.size = '' #_sizeRange[0]
+        self.age_range = '' #_age_range[0]
+        self.size_range = '' #_size_range[0]
         self.weight = 0
         self.sex = '' #_sex[0]
 
@@ -35,10 +35,21 @@
 class Dog(Animal):
     """description of Dog goes here"""
 
+    from datetime import datetime
+
     _group = ['Hearding','Hound','NonSporting','Sporting','Terrier','Toy','Working','FoundationStockService','Miscellaneous']
 
     def __init__(self):
+
+        _time = self.datetime.utcnow().replace(microsecond = 0).isoformat()
+
         super().__init__()
+        self.status = ''
         self.breed = ''
         self.group = '' #_group[0]
         self.color = ''
+        self.good_with = {'children': False, 'dogs': False, 'cats': False}
+        self.trained = False
+        self.notes = ''
+        self.links = []
+        self.metadata = {'created': _time, 'updated': _time}
