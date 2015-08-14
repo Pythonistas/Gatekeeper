768ba2563f91428746db602efbda98911a68c24a
Skip Breidbach <skip@waywally.com>
2015-08-13 19:53:35 -0700

    Dogs! (plural)

--- Gatekeeper/model/animal.py
+++ Gatekeeper/model/animal.py
@@ -4,8 +4,12 @@ from marshmallow import Schema, fields
 from flask import request
 from datetime import datetime
 
-
 api = Api(app, prefix='/api/v1')
+
+
+def fields_from_request(request):
+    fields = request.args.get('fields')
+    return fields.split(',') if fields else None
 
 
 class Animal(Resource):
@@ -14,10 +18,10 @@ class Animal(Resource):
         name = fields.Str()
         birth_date = fields.Date()
 
-    def get(self):
-        fields = request.args.get('fields')
-        fields_list = fields.split(',') if fields else None
-        schema = self._Schema(only=fields_list)
+    def get(self, object_id):
+        self.object_id = object_id
+
+        schema = self._Schema(only=fields_from_request(request))
         data, errors = schema.dump(self)
         return errors if errors else data
 
@@ -26,10 +30,30 @@ class Dog(Animal):
 
     class _Schema(Animal._Schema):
         breed = fields.Str()
+        url = fields.Str()
 
-    def __init__(self):
+    def __init__(self, object_id=None):
+        self.object_id = object_id
         self.name = 'fred'
         self.breed = 'bison'
         self.birth_date = datetime.now()
 
-api.add_resource(Dog, '/dog/')
+    @property
+    def url(self):
+        return request.url
+
+
+class Dogs(Resource):
+
+    @property
+    def dogs(self):
+        return [Dog(i) for i in range(5)]
+
+    def get(self):
+        schema = Dog._Schema(only=fields_from_request(request), many=True)
+        data, errors = schema.dump(self.dogs)
+        return errors if errors else {'dogs': data}
+
+
+api.add_resource(Dog, '/dogs/<int:object_id>')
+api.add_resource(Dogs, '/dogs/')
