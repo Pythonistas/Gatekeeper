27c550ed1124d6281d4e1b924d9ac67e42c7f088
Skip Breidbach <skip@waywally.com>
2015-08-19 08:12:31 -0700

    Revert "Merge branch 'feature/JSON_Output' into develop"
    This reverts commit 47d4611b395e242f81ca4c4602653847513fadac, reversing
    changes made to 021cba606ceb5bb1878131b2fc07765fe0a205ff.

--- Gatekeeper/__init__.py
+++ Gatekeeper/__init__.py
@@ -1,13 +1,8 @@
-﻿"""
+"""
 The flask application package.
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
-app.config['ERROR_404_HELP'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-
 import Gatekeeper.views
-import Gatekeeper.model.animal
