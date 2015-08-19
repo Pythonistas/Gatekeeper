1965623fb1ac5196944655c821cd63005721c84b
Skip Breidbach <skip@waywally.com>
2015-08-19 08:18:35 -0700

    Revert

--- Gatekeeper/__init__.py
+++ Gatekeeper/__init__.py
@@ -1,8 +1,13 @@
-"""
+﻿"""
 The flask application package.
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
+app.config['ERROR_404_HELP'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
 import Gatekeeper.views
+import Gatekeeper.models
