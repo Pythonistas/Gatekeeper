9e320e4ddeb495a9bd43afbb0fd4680183997429
Lorraine Rapp <rapp@waywally.com>
2015-08-22 08:55:30 -0700

    updated __init__.py to use config framework

--- Gatekeeper/__init__.py
+++ Gatekeeper/__init__.py
@@ -1,13 +1,18 @@
 ﻿"""
 The flask application package.
 """
+from os import environ
 
 from flask import Flask
 
+from Gatekeeper import gatekeeper_config
+
 app = Flask(__name__)
 
-app.config['ERROR_404_HELP'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
+if 'CONFIG_MODE' in environ:
+    app.config.from_object(eval(gatekeeper_config.config_dict[environ['CONFIG_MODE']]))
+else:
+    app.config.from_object(eval(gatekeeper_config.config_dict['DEFAULT']))
 
 import Gatekeeper.views
 import Gatekeeper.model.animal
