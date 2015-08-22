0d8d1a3abc073eeb04904d93603306c71aaaa839
Lorraine Rapp <rapp@waywally.com>
2015-08-22 08:49:36 -0700

    Implementation of configuration framework

---
+++ Gatekeeper/gatekeeper_config.py
@@ -0,0 +1,18 @@
+﻿# Note for Visual Studio: After adding CONFIG_MODE to the environment, need to close and restart Visual Studio for setting to take effect
+
+config_dict = {'DEFAULT' : 'gatekeeper_config.Config', 
+               'DEBUG'   : 'gatekeeper_config.DevelopmentConfig',
+               'TEST'    : 'gatekeeper_config.TestingConfig'
+               }
+
+class Config(object):
+    ERROR_404_HELP = False
+    PROPAGATE_EXCEPTIONS = True
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
