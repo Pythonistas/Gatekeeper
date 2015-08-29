1aa298274f66ece049ad9263ee6d6db003333ce9
Lorraine Rapp <rapp@waywally.com>
2015-08-29 07:35:51 -0700

    Genericized variable naming in load_from_yaml

--- Gatekeeper/model/util.py
+++ Gatekeeper/model/util.py
@@ -4,15 +4,15 @@ def fields_from_request(request):
     fields = request.args.get('fields')
     return fields.split(',') if fields else None
 
-def load_from_yaml(input_file, animal_class):
-    animals = {}
+def load_from_yaml(input_file, data_class):
+    yaml_dict = {}
     try:
         with open(input_file, 'r') as stream:
             for data in yaml.safe_load(stream):
-                cur_animal = animal_class()
+                cur_item = data_class()
                 for key, value in data.items():
-                    setattr(cur_animal, key, value)
-                animals[cur_animal.object_id] = cur_animal
+                    setattr(cur_item, key, value)
+                yaml_dict[cur_item.object_id] = cur_item
     except OSError:
         return None
-    return animals
+    return yaml_dict
