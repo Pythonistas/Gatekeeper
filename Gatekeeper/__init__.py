373ab3e4709a48fa8963208dc92f4011d5abd907
Chris Phillips <crphillip@hotmail.com>
2015-09-08 22:30:11 -0700

    Temporarily removing extra whitespace from generated HTML.
    See link for more details:
    http://jinja.pocoo.org/docs/dev/templates/#whitespace-control
    
    To-Do:
    Integrate with Flask application configuration

--- Gatekeeper/__init__.py
+++ Gatekeeper/__init__.py
@@ -10,6 +10,8 @@ from Gatekeeper import gatekeeper_config
 app = Flask(__name__)
 
 app.config.from_object(gatekeeper_config.config_dict[environ.get('CONFIG_MODE', 'DEFAULT')])
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 import Gatekeeper.views
 import Gatekeeper.model.animal
