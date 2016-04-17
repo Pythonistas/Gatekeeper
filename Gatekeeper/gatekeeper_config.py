965c6752766d5f811aff1218601fb3b3e271235c
Skip Breidbach <skip@waywally.com>
2016-04-17 13:36:58 -0700

    hide sql alchemy toolbar

--- Gatekeeper/gatekeeper_config.py
+++ Gatekeeper/gatekeeper_config.py
@@ -12,6 +12,17 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = 'they killed kenny'
+    DEBUG_TB_PANELS = [
+      'flask_debugtoolbar.panels.versions.VersionDebugPanel',
+      'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+      'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+      'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
+      'flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel',
+      'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+      'flask_debugtoolbar.panels.logger.LoggingPanel',
+      'flask_debugtoolbar.panels.route_list.RouteListDebugPanel',
+      'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel'
+    ]
     DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
 
 
